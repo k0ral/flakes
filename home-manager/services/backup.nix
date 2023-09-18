6f34d3f0e3a49f2240cea366545eaa1ea6eb94e4
koral <mail@cmoreau.info>
2023-09-18 23:15:40 +0200

    fix: Borg option --paths-from-stdin actually does not recurse

--- home-manager/services/backup.nix
+++ home-manager/services/backup.nix
@@ -1,26 +1,27 @@
-{ config, pkgs ? import <nixpkgs> { }, ... }:
+{ config, lib, pkgs ? import <nixpkgs> { }, ... }:
 
 let
-  borg-paths = pkgs.writeText "borg_paths" ''
-    ${config.home.homeDirectory}/digital-garden
-    ${config.home.homeDirectory}/doc
-    ${config.home.homeDirectory}/finance
-    ${config.home.homeDirectory}/flakes
-    ${config.home.homeDirectory}/foundry
-    ${config.home.homeDirectory}/images
-    ${config.home.homeDirectory}/papers
-    ${config.home.homeDirectory}/prog/archive
-    ${config.home.homeDirectory}/services
-    ${config.home.homeDirectory}/.thunderbird
-    ${config.home.homeDirectory}/.local/share/gtg
-    ${config.home.homeDirectory}/.password-store
-    ${config.xdg.configHome}
-  '';
+  borg-paths = [
+    "${config.home.homeDirectory}/digital-garden"
+    "${config.home.homeDirectory}/doc"
+    "${config.home.homeDirectory}/finance"
+    "${config.home.homeDirectory}/flakes"
+    "${config.home.homeDirectory}/foundry"
+    "${config.home.homeDirectory}/images"
+    "${config.home.homeDirectory}/papers"
+    "${config.home.homeDirectory}/prog/archive"
+    "${config.home.homeDirectory}/services"
+    "${config.home.homeDirectory}/.librewolf"
+    "${config.home.homeDirectory}/.local/share/gtg"
+    "${config.home.homeDirectory}/.password-store"
+    "${config.home.homeDirectory}/.thunderbird"
+    "${config.xdg.configHome}"
+  ];
   backupScript = pkgs.writeShellScriptBin "backup" ''
     export BORG_PASSCOMMAND="${pkgs.coreutils}/bin/cat ${config.xdg.configHome}/secrets/borg-passphrase"
     REPOSITORY=$(${pkgs.coreutils}/bin/cat ${config.xdg.configHome}/secrets/borg-repository)
 
-    ${pkgs.borgbackup}/bin/borg create --paths-from-stdin --remote-path=borg1 -x -C lzma "$REPOSITORY:backup::{utcnow}-{hostname}" < ${borg-paths}
+    ${pkgs.borgbackup}/bin/borg create --remote-path=borg1 -x -C lzma "$REPOSITORY:backup::{utcnow}-{hostname}" ${lib.strings.concatStringsSep " " borg-paths}
     ${pkgs.borgbackup}/bin/borg prune --remote-path=borg1 -d 30 "$REPOSITORY:backup"
   '';
 in {
