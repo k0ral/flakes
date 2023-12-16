919c63dbd5ac9ae6aefe97c2b708498fc19d468f
koral <mail@cmoreau.info>
2023-12-16 18:14:17 +0100

    feat(neovim): Print relative path to file in status bar

--- hm-modules/neovim/lua/main/init.lua
+++ hm-modules/neovim/lua/main/init.lua
@@ -146,7 +146,7 @@ require('lualine').setup {
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', { 'diagnostics', sources = { 'nvim_lsp' } } },
-    lualine_c = {'filename'},
+    lualine_c = {{'filename', path = 1}},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
