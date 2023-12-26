9a9efc288a4fb62295e147cea993a14c260efbe1
koral <mail@cmoreau.info>
2023-12-26 13:45:11 +0100

    feat(neovim): Switch from `paq` to `lazy` package manager

--- hm-modules/neovim/lua/plugins/init.lua
+++ hm-modules/neovim/lua/plugins/init.lua
@@ -1,49 +1,52 @@
-local cmd = vim.cmd
-local fn = vim.fn
-
-local install_path = fn.stdpath('data')..'/site/pack/paqs/start/paq-nvim'
-
-if fn.empty(fn.glob(install_path)) > 0 then
-  cmd('!git clone --depth 1 https://github.com/savq/paq-nvim.git '..install_path)
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
-require "paq" {
-  "savq/paq-nvim";  -- Let Paq manage itself
-
-  'andymass/vim-matchup';
-  'echasnovski/mini.nvim';
-  'folke/trouble.nvim';
-  'folke/which-key.nvim';
-  'ggandor/leap.nvim';
-  'hrsh7th/nvim-cmp';
-  'hrsh7th/cmp-nvim-lsp';
-  'hrsh7th/cmp-nvim-lsp-signature-help';
-  'j-hui/fidget.nvim';
-  'johmsalas/text-case.nvim';
-  'jose-elias-alvarez/null-ls.nvim';
-  'kyazdani42/nvim-web-devicons';
-  'L3MON4D3/LuaSnip';
-  'lervag/vimtex';
-  'lewis6991/gitsigns.nvim';
-  'lukas-reineke/indent-blankline.nvim';
-  'mbbill/undotree';
-  'mcauley-penney/tidy.nvim';
-  'mickael-menu/zk-nvim';
-  'neovim/nvim-lspconfig';
-  'neovimhaskell/haskell-vim';
-  'numToStr/Comment.nvim';
-  'nvim-lua/popup.nvim';
-  'nvim-lua/plenary.nvim';
-  'nvim-lualine/lualine.nvim';
-  'nvim-telescope/telescope.nvim';
-  {'nvim-treesitter/nvim-treesitter', build = 'TSUpdate'};
-  'marko-cerovac/material.nvim';
-  'p00f/nvim-ts-rainbow';
-  'purescript-contrib/purescript-vim';
-  'ray-x/go.nvim';
-  'romgrk/barbar.nvim';
-  'saadparwaiz1/cmp_luasnip';
-  'stevearc/dressing.nvim';
-  'tpope/vim-eunuch';
-  'vmchale/dhall-vim';
-}
+require("lazy").setup({
+  "andymass/vim-matchup",
+  "echasnovski/mini.nvim",
+  "folke/trouble.nvim",
+  "folke/which-key.nvim",
+  "ggandor/leap.nvim",
+  "hrsh7th/nvim-cmp",
+  "hrsh7th/cmp-nvim-lsp",
+  "hrsh7th/cmp-nvim-lsp-signature-help",
+  "j-hui/fidget.nvim",
+  "johmsalas/text-case.nvim",
+  "jose-elias-alvarez/null-ls.nvim",
+  "kyazdani42/nvim-web-devicons",
+  "L3MON4D3/LuaSnip",
+  "lervag/vimtex",
+  "lewis6991/gitsigns.nvim",
+  "lukas-reineke/indent-blankline.nvim",
+  "mbbill/undotree",
+  "mcauley-penney/tidy.nvim",
+  "mickael-menu/zk-nvim",
+  "neovim/nvim-lspconfig",
+  "neovimhaskell/haskell-vim",
+  "numToStr/Comment.nvim",
+  "nvim-lua/popup.nvim",
+  "nvim-lua/plenary.nvim",
+  "nvim-lualine/lualine.nvim",
+  "nvim-telescope/telescope.nvim",
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+  "marko-cerovac/material.nvim",
+  "p00f/nvim-ts-rainbow",
+  "purescript-contrib/purescript-vim",
+  "ray-x/go.nvim",
+  "romgrk/barbar.nvim",
+  "saadparwaiz1/cmp_luasnip",
+  "simrat39/rust-tools.nvim",
+  "stevearc/dressing.nvim",
+  "tpope/vim-eunuch",
+  "vmchale/dhall-vim",
+})
