c31576dd9e2f2654d51942d7dbb698cc120231f8
koral <mail@cmoreau.info>
2023-12-28 16:39:50 +0100

    feat(neovim): Install `aerial` plugin

--- hm-modules/neovim/lua/main/init.lua
+++ hm-modules/neovim/lua/main/init.lua
@@ -54,6 +54,11 @@ cmd 'colorscheme material'
 -- Plugin specific
 --
 
+-- Aerial
+require("aerial").setup({
+  open_automatic = true,
+})
+
 -- Comment
 require('Comment').setup()
 
@@ -137,7 +142,7 @@ require('lualine').setup {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', { 'diagnostics', sources = { 'nvim_lsp' } } },
     lualine_c = {{'filename', path = 1}},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_x = {'aerial', 'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
   },
